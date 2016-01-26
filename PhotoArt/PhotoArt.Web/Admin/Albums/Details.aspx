b9b1784db7d2d9ad7325ff5318c2f0a72344490c
MariyaSteffanova <mariya.steffanova@gmail.com>
2016-01-26 06:13:55 +0200

    Basic details page added

---
+++ PhotoArt/PhotoArt.Web/Admin/Albums/Details.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Details.aspx.cs" Inherits="PhotoArt.Web.Admin.Albums.Details" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Details</h1>
+    <asp:Label ID="ImagUrlLabel" runat="server" />
+    <asp:Image ID="ImageContainer" runat="server" />
+</asp:Content>
---
+++ PhotoArt/PhotoArt.Web/Admin/Albums/Details.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhotoArt.Web.Admin.Albums
+{
+    public partial class Details : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var id = int.Parse(Request.Params["Id"]);
+            if (Request.Params["Id"] == null)
+            {
+                Response.Redirect("GridViewDemo.aspx");
+            }
+
+            var image = this.Data.Albums
+                .FirstOrDefault(x => x.Id == id)
+                .Images
+                .FirstOrDefault();
+              
+
+            this.ImagUrlLabel.Text = image.OriginalName;
+            this.ImageContainer.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String(image.Content);
+        }
+    }
+}
