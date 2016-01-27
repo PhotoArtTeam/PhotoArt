060a5b4753f935f10964c4b353aa520a3685a3e6
Margarita Ivancheva <mvivancheva@gmail.com>
2016-01-27 19:37:32 +0200

    Added Update panel

---
+++ PhotoArt/Web/PhotoArt.Web/Albums/Update.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Update.aspx.cs" Inherits="PhotoArt.Web.Admin.Albums.Update" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+</asp:Content>
---
+++ PhotoArt/Web/PhotoArt.Web/Albums/Update.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhotoArt.Web.Admin.Albums
+{
+    public partial class Update : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var id = int.Parse(Request.Params["Id"]);
+
+
+        }
+    }
+}
