d9255f1e160baaae2ff433c58713b6966cdbad62
Margarita Ivancheva <mvivancheva@gmail.com>
2016-01-28 13:23:23 +0200

    added admin roles

--- PhotoArt/Data/PhotoArt.Data/Migrations/Configuration.cs
+++ PhotoArt/Data/PhotoArt.Data/Migrations/Configuration.cs
@@ -33,6 +33,28 @@ namespace PhotoArt.Data.Migrations
             {
                 EnumTablesHelper.SeedEnumData<TechnologyType, TechnologyTypeEnum>(context.TechnologyTypes);
             }
+            if (!context.Users.Where(u => u.UserName == "admin@admin.com").Any())
+            {
+                var admin = new User
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                    PasswordHash = "AJ6xY/N9kWRShbJYh8QE5fIt5Yg5IeyTFBnJ9RXAdIXUo9hiqxtVWRiYJ0XyUHVDbg==",
+                    SecurityStamp = "59bcf0ca-0367-4397-b3c1-af413e479e0c",
+                    LockoutEnabled = true,
+                };
+
+                context.Users.Add(admin);
+
+                var role = context.Roles.Where(r => r.Name == "Admin").FirstOrDefault();
+                var adminRole = new IdentityUserRole
+                {
+                    RoleId = role.Id
+                };
+
+                admin.Roles.Add(adminRole);
+                context.SaveChanges();
+            }
         }
     }
 }
