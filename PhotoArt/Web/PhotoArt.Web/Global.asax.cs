d5e478224055fbf3adebf9e9a0255b7ca30aed2b
Margarita Ivancheva <mvivancheva@gmail.com>
2016-01-28 16:19:00 +0200

    added 404 page not found

--- PhotoArt/Web/PhotoArt.Web/Global.asax.cs
+++ PhotoArt/Web/PhotoArt.Web/Global.asax.cs
@@ -17,5 +17,21 @@ namespace PhotoArt.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            Exception lastError = application.Server.GetLastError();
+            HttpException ex = lastError as HttpException;
+            string fourOhFourPage = "~/PageNotFound.aspx";
+            // Only response to 404 errors
+            if (ex != null && ex.GetHttpCode() == 404)
+            {
+                // Clear the error in order to avoid standard handling of the error.
+                application.Server.ClearError();
+                application.Context.Handler =
+                      System.Web.UI.PageParser.GetCompiledPageInstance(fourOhFourPage,
+                      application.Server.MapPath(fourOhFourPage), application.Context);
+            }
+        }
     }
 }
